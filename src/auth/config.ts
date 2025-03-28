f91e000a7b2cba894c7eb31dd07faa140cfd34f1
Matheus Littig <littig.works@gmail.com>
2025-03-28 18:31:55 -0400

    chore: adding drizzle schemas to better auth config

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,4 +1,10 @@
 import { db } from "@server/database";
+import {
+  accountTable,
+  sessionTable,
+  userTable,
+  verificationTable,
+} from "@server/database/schema";
 import { env } from "@server/utils/env";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
@@ -10,6 +16,12 @@ export const auth = betterAuth({
   plugins: [bearer()],
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: {
+      user: userTable,
+      account: accountTable,
+      verification: verificationTable,
+      session: sessionTable,
+    },
   }),
   emailAndPassword: {
     enabled: true,
